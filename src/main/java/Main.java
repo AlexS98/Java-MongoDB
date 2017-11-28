755371e12ec9ff5ecade99e3c9c750c2a4f49390
Alexander <sania.sviridenko@gmail.com>
2017-11-28 23:40:10 +0200

    add travis

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,46 +1,55 @@
 import com.mongodb.*;
 
-import com.mongodb.client.FindIterable;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.MongoCollection;
+import com.mongodb.client.*;
 
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Updates;
 import org.bson.Document;
-import java.util.*;
-
-import com.mongodb.client.MongoCursor;
-import static com.mongodb.client.model.Filters.*;
-import com.mongodb.client.result.DeleteResult;
-import static com.mongodb.client.model.Updates.*;
-import com.mongodb.client.result.UpdateResult;
 
 import java.io.IOException;
+import java.util.*;
+
+import static com.mongodb.client.model.Filters.eq;
 
 public class Main {
-    public static void main(String args[]){
+    public static void main(String args[]) throws IOException {
         MongoClient mongo = new MongoClient( "localhost" , 27017 );
-
-        MongoCredential credential;
-        credential = MongoCredential.createCredential("sampleUser", "myDb",
-                "password".toCharArray());
         System.out.println("Connected to the database successfully");
 
-        MongoDatabase database = mongo.getDatabase("myDb");
+        MongoDatabase database = mongo.getDatabase("logDB");
 
-        MongoCollection<Document> collection = database.getCollection("sampleCollection");
-        System.out.println("Collection myCollection selected successfully");
-
-        collection.updateOne(Filters.eq("id", 1), Updates.set("likes", 150));
-        System.out.println("Document update successfully...");
-
-        FindIterable<Document> iterDoc = collection.find();
-        int i = 1;
-
-        Iterator it = iterDoc.iterator();
-        while (it.hasNext()) {
-            System.out.println(it.next());
-            i++;
+        MongoCollection<Document> collection = database.getCollection("logs");
+        LogEntry[] logs = new LogEntry[] {
+                new LogEntry("mySite1.com","192.168.0.1",new Date(),122),
+                new LogEntry("mySite2.com","192.168.0.2",new Date(),562),
+                new LogEntry("mySite3.com","192.168.0.3",new Date(),781),
+                new LogEntry("mySite4.com","192.168.0.4",new Date(),1818),
+                new LogEntry("mySite5.com","192.168.0.5",new Date(),163)
+        };
+        /*
+        for (LogEntry log: logs) {
+            collection.insertOne(convertToDoc(log));
         }
+        for(int i = 0; i < logs.length; i++) {
+            collection.deleteOne(eq("URL", "mySite" + (i+1) +".com"));
+        }
+        */
+        MongoCursor<Document> cursor = collection.find().iterator();
+        try {
+            while (cursor.hasNext()) {
+                System.out.println(cursor.next());
+            }
+        }finally {
+            cursor.close();
+        }
+
+        mongo.close();
+    }
+
+    private static Document convertToDoc(LogEntry myLog){
+        return new Document("URL", myLog.entryURL)
+                .append("IP",myLog.entryIP)
+                .append("Date", myLog.entryDate)
+                .append("Time", myLog.entryTimeSeconds);
     }
 }
