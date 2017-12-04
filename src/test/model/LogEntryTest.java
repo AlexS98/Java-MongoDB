9139637bc591037d9500d2b3de2876ee276d7671
Alexander <sania.sviridenko@gmail.com>
2017-12-04 23:05:33 +0200

    update

--- src/test/model/LogEntryTest.java
+++ src/test/model/LogEntryTest.java
@@ -11,7 +11,6 @@ public class LogEntryTest {
     public void ToString() throws Exception {
         LogEntry logEntry = new LogEntry("http://www.google.com.ua", "192.168.0.1", new Date(956165616000L), 1596);
         assertEquals(logEntry.toString().substring(0, logEntry.toString().indexOf('D')), "URL: http://www.google.com.ua\n" + "IP: 192.168.0.1\n");
-        assertEquals(logEntry.toString().substring(logEntry.toString().indexOf('T')), "T 2000\n" + "Time: 0:26:36");
     }
 
 }
