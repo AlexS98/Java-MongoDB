acea9f2779c163c52aa8f5151d686e531eeaee9e
Alexander <sania.sviridenko@gmail.com>
2017-12-04 22:52:27 +0200

    fix issues

--- src/test/model/LogEntryTest.java
+++ src/test/model/LogEntryTest.java
@@ -9,8 +9,8 @@ import static org.junit.Assert.*;
 public class LogEntryTest {
     @Test
     public void ToString() throws Exception {
-        LogEntry logEntry = new LogEntry("http://www.google.com.ua", "192.168.0.1", new Date(), 1596);
-        assertEquals(logEntry.toString(), "");
+        LogEntry logEntry = new LogEntry("http://www.google.com.ua", "192.168.0.1", new Date(956165616000L), 1596);
+        assertEquals(logEntry.toString(), "URL: http://www.google.com.ua\n" + "IP: 192.168.0.1\n" + "Date: Wed Apr 19 20:33:36 EEST 2000\n" + "Time: 4:2:36");
     }
 
 }
